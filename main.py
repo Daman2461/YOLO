4291cd8fc1c52f6fbdc46c905ff61e585a6ed2f2
Daman2461 <142651012+Daman2461@users.noreply.github.com>
2024-08-31 22:51:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
- import cv2
+import cv2
 import numpy as np
 
 net = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
@@ -8,49 +8,42 @@ classes = []
 with open('coco.names', 'r') as f:
     classes = [line.strip() for line in f.readlines()]
 
-cap = cv2.VideoCapture(0)
+image = cv2.imread('./input.jpg')
+height, width, channels = image.shape
+blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+net.setInput(blob)
+outs = net.forward(output_layers)
+class_ids = []
+confidences = []
+boxes = []
 
-while True:
-    _, frame = cap.read()
-    height, width, channels = frame.shape
-    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-    net.setInput(blob)
-    outs = net.forward(output_layers)
-    class_ids = []
-    confidences = []
-    boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
 
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                center_x = int(detection[0] * width)
-                center_y = int(detection[1] * height)
-                w = int(detection[2] * width)
-                h = int(detection[3] * height)
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-                boxes.append([x, y, w, h])
-                confidences.append(float(confidence))
-                class_ids.append(class_id)
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+if len(indexes) > 0:
+    for i in indexes.flatten():
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        confidence = str(round(confidences[i], 2))
+        color = (0, 255, 0)
+        cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(image, label + " " + confidence, (x, y + 20), cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
 
-    if len(indexes) > 0:
-        for i in indexes.flatten():
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
-            confidence = str(round(confidences[i], 2))
-            color = (0, 255, 0)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(frame, label + " " + confidence, (x, y + 20), cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
-
-    cv2.imshow('Image', frame)
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
-
-cap.release()
+cv2.imshow('Image', image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
